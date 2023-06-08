86223111c213748c61e74f2d6f68a170e2c196d3
melshinawy <melshinawy@users.noreply.github.com>
2023-06-08 21:38:34 +0300

    Attempt to centralize all posts using js

--- network/static/network/helper.js
+++ network/static/network/helper.js
@@ -1,4 +1,5 @@
-function createPost(posts) {
+
+// function createPost(posts) {
     let divCard = document.createElement('div');
     let divCardHeader = document.createElement('div');
     let divCardBody = document.createElement('div');
@@ -7,16 +8,62 @@ function createPost(posts) {
     divCardHeader.className = "card-header";
     divCardBody.className = "card-body";
 
-    posts.forEach(post => {
-        let a = document.createElement('a');
-        a.innerHTML = post.user;
-        a.href = "{% url 'profile' post.user.id %}";
+//     posts.forEach(post => {
+//         let page = `
+//         <div class="container-fluid">
+//         <h2>All Posts</h2>`
 
-        divCardHeader.append(a)
+//         {% if user.is_authenticated %}
+//             <div class="card">
+//                 <form action="{% url 'index' %}" method="post">
+//                     {% csrf_token %}
+//                     <div class="form-group" style="margin: 20px">
+//                         <label for="new-post"><h5>New Post</h5></label><br>
+//                         <textarea class="form-control mb-3" name="new-post"></textarea>
+//                         <button class="btn btn-primary" type="submit">Post</button>
+//                     </div>
+//                 </form>
+//             </div>
+//         {% endif %}
+    
+//         <div class="card">
+//             {% for post in posts reversed %}
+//                 <div class="card-header">
+//                     <a href="{% url 'profile' post.user.id %}">{{  post.user }}</a>
+//                 </div>
+//                 <div class="card-body">
+//                     <blockquote class="blockquote mb-0">
+//                         {% if user.id == post.user.id %}
+//                             <a href="#"><h6 style="font-size: 12px; text-align: right;">Edit</h6></a>
+//                         {% endif %}
+//                         <p>{{  post.content }}</p>
+//                         <i class="bi bi-heart" style="color: red;"></i>
+//                         <small> {{ post.likes.count }}</small><br>
+//                         <footer class="footer" style="font-size: 12px; text-align: right;">{{ post.timestamp }}</footer>
+//                     </blockquote>
+                    
+//                 </div>
+//                 {% endfor %}
+//         </div>
+//     </div>    
+//   `
+        
+        
+        
+        
+        
+        
+        
+        
+//     // let a = document.createElement('a');
+//         // a.innerHTML = post.user;
+//         // a.href = "{% url 'profile' post.user.id %}";
 
-        let blockQuote = document.createElement('blockquote');
-        blockQuote.className = "blockquote mb-0";
+//         // divCardHeader.append(a)
 
-    })  
+//         // let blockQuote = document.createElement('blockquote');
+//         // blockQuote.className = "blockquote mb-0";
 
-}
+//     })  
+
+// }
