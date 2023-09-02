02997c49a558f01e189a866ed00b8baacf7221c0
melshinawy <melshinawy@users.noreply.github.com>
2023-09-02 21:44:17 +0300

    Pagination implementation - added comments - code cleanup

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -1,68 +1,80 @@
 document.addEventListener('DOMContentLoaded', 
     function () {
+        // Declare DOM objects to be used
+        const followBtn = document.querySelector('button'); // Follow button in profile page. Used to follow/unfollow user
+        const editLink = document.querySelectorAll('.edit'); // Link used for editing a post
+        const hearts = document.querySelectorAll('.bi-heart'); // Empty hearts to indicate that a post is not liked by a user
+        const filledHearts = document.querySelectorAll('.bi-heart-fill'); // Filled hearts to indicate that a post is liked by a user
+        const userNav = document.querySelector('#user-nav'); // Nav link for logged user to indicate that user is authenticated
 
-        const followBtn = document.querySelector('button');
-        const editLink = document.querySelectorAll('.edit');
-        const hearts = document.querySelectorAll('.bi-heart');
-        const filledHearts = document.querySelectorAll('.bi-heart-fill');
-        const userNav = document.querySelector('#user-nav');
-
+        // If no user is logged skip running the script
         if (!userNav ) {
             return;
         }
 
+        // csrf token declaration used for POST/PUT methods
         const csrftoken = userNav.dataset.csrftoken;
 
+        // Format each heart and filled heart to determine their onclick, onmouseover and onmouseout behaviour
         hearts.forEach(heart => setHeartStyle(heart));
         filledHearts.forEach(filledHeart => setHeartStyle(filledHeart));
         
+        // Format each edit link to determine their onclick, onmouseover and onmouseout behaviour
         if(editLink) {
             editLink.forEach(link => {
                 link.addEventListener('click', () => editPost(link.parentNode));
             })
         }
 
+        // Create the onclick event for button used to follow/unfollow a user when on a user profile page
         if (followBtn) {
             followBtn.addEventListener('click', () => toggleFollowBtn())
         }
 
+        // Function used to style hearts based on their class
         function setHeartStyle(heart) {
 
+            // When a heart is not filled (post not liked)
             if (heart.className === 'bi bi-heart h6'){
+                // Fill heart and add a pointer onmouseover
                 heart.addEventListener('mouseover', () => {
                     heart.className = 'bi bi-heart-fill h6';
                     heart.style.cursor = 'pointer';
                 })
-
+                // Bring the heart to original state onmouseout
                 heart.addEventListener('mouseout', () => {
                     heart.className = 'bi bi-heart h6';
                 })
-
+                // Run updateLikes function when the heart is clicked to add a like to the post
                 heart.addEventListener('click', () => {
                     updateLikes(heart.parentNode, 'like', csrftoken);
                 })
+            // When a heart is filled (post liked)
             } else {
+                // Empty heart and add a pointer onmouseover
                 heart.addEventListener('mouseover', () => {
                     heart.className = 'bi bi-heart h6';
                     heart.style.cursor = 'pointer';
                 })
-    
+                // Bring the heart to original state onmouseout
                 heart.addEventListener('mouseout', () => {
                     heart.className = 'bi bi-heart-fill h6';
                 })
-                
+                // Run updateLikes function when the heart is clicked remove like from the post
                 heart.addEventListener('click', () => {
                     updateLikes(heart.parentNode, 'unlike', csrftoken);
                 })
             }
         }
-        
+        // Function used to set behaviour when an edit link is clicked
         function editPost(blockQuote) {
-            const divCardBody = blockQuote.parentNode
-            const editForm = document.createElement('div');
-            const postContent = blockQuote.dataset.postContent;
-            const postId = blockQuote.dataset.postId;
-    
+            // DOM elements declarations
+            const divCardBody = blockQuote.parentNode // Parent div containing the post
+            const editForm = document.createElement('div'); // Create an edit form to change post content
+            const postContent = blockQuote.dataset.postContent; // Original post content
+            const postId = blockQuote.dataset.postId; // Post id in database
+
+            // Set editForm parameters and styling
             editForm.className="card";
             editForm.innerHTML = 
             `
@@ -74,18 +86,23 @@ document.addEventListener('DOMContentLoaded',
                     </div>
                 </form>
             `
-            blockQuote.style.display = 'none';
-            divCardBody.appendChild(editForm);
-    
+
+            blockQuote.style.display = 'none'; // Hide original post content
+            divCardBody.appendChild(editForm); // Append newly created editForm to parent div
+            
+            // Declare post and cancel buttons to determine their behaviour when clicked
             const postBtn = document.querySelector('#post');
             const cancelBtn = document.querySelector('#cancel');
-    
+            
+            // When cancel button is clicked
             cancelBtn.onclick = function() {
-                editForm.remove();
-                blockQuote.style.display = 'block';    
+                editForm.remove(); // Delete form
+                blockQuote.style.display = 'block'; // Display original post    
             }
-    
+            
+            // When post button is clicked
             postBtn.onclick = function () {
+                // Update database content through edit_post
                 fetch(`/edit_post`, {
                     method: 'PUT',
                     headers: {'X-CSRFToken': csrftoken},
@@ -95,25 +112,22 @@ document.addEventListener('DOMContentLoaded',
                     })})
                 .then(response => response.json())
                 .then(post => {
-                    console.log(post)
+                    // If a post is returned
                     if (post){
+                        blockQuote.dataset.postContent = post.content; // Update post content in object's dataset
                         
-                        blockQuote.dataset.postContent = post.content;
-                        
-                        blockQuote.querySelector('.post-text').innerHTML = post.content;
-                        blockQuote.querySelector('footer').innerHTML = ` Posted on: ${post.timestamp} - Last Updated: ${post.last_update}`;
+                        blockQuote.querySelector('.post-text').innerHTML = post.content; // Update post content on page
+                        blockQuote.querySelector('footer').innerHTML = ` Posted on: ${post.timestamp} - Last Updated: ${post.last_update}`; // Add/update last updated text
 
-                        editForm.remove();
-                        blockQuote.style.display = 'block';
-                        
-                }
-    
-                })
+                        editForm.remove(); // remove edit form
+                        blockQuote.style.display = 'block'; // display post with new content                       
+                } 
+                });
             }
         }
-
+        // Function used to toggle follow button on profile page
         function toggleFollowBtn() {
-            
+            // Update database with follow data through edit_follow
             fetch(`/edit_follow`, {
                 method: 'PUT',
                 headers: {'X-CSRFToken': followBtn.dataset.csrftoken},
@@ -124,6 +138,7 @@ document.addEventListener('DOMContentLoaded',
             })
             .then(response => response.json())
             .then(result => {
+                // Change follow button style and following/follower info to reflect updated follow status
                 if (followBtn.id === 'follow') {
                     followBtn.id = 'unfollow';
                     followBtn.innerHTML = 'Unfollow';
@@ -139,10 +154,11 @@ document.addEventListener('DOMContentLoaded',
                     let numFollowers = document.querySelector("#followers");
                     numFollowers.innerHTML = (parseInt(numFollowers.innerHTML) - 1).toString();
                 }
-            })
+            });
         }
-
+        // Function used to update post likes
         function updateLikes(heartParentNode, modification, csrftoken) {
+            // Update database with likes data through edit_like
             fetch(`/edit_like`, {
                 method: 'PUT',
                 headers: {'X-CSRFToken': csrftoken},
@@ -153,17 +169,18 @@ document.addEventListener('DOMContentLoaded',
                 })
                 .then(response => response.json())
                 .then(post => {
+                    // Create a new heart based on like status
                     const heart = heartParentNode.querySelector('i');
                     const updatedHeart = heart.cloneNode(false);
-                    
+                    // Update heart style based on like status
                     if (post.likers.includes(Number(heartParentNode.dataset.userId))){
                         updatedHeart.className = 'bi bi-heart-fill h6';
                     } else {
                         updatedHeart.className = 'bi bi-heart h6';
                     }
-                    setHeartStyle(updatedHeart);
-                    heartParentNode.querySelector('small').innerHTML = post.likes;
-                    heart.replaceWith(updatedHeart);               
-            })
+                    setHeartStyle(updatedHeart); // Update heart events based on like status
+                    heartParentNode.querySelector('small').innerHTML = post.likes; // Update like count
+                    heart.replaceWith(updatedHeart); // Replace old heart with new one
+            });
         }
 })
--- network/views.py
+++ network/views.py
@@ -11,124 +11,141 @@ from .models import User, Post, Follow
 
 from .models import User
 
-
+# View for 'all posts'
 def index(request):
+    # If request method is GET
     if request.method == 'GET':
-        posts = Post.objects.all()
-        serialized_posts = [post.serialize() for post in posts]
-
-        p = Paginator(serialized_posts, 2)
-        page = request.GET.get('page')
-        paginated_posts = p.get_page(page)
-
+        posts = Post.objects.all().order_by("-timestamp") # Get all posts ordered by timestamp descending (new to old)
+        paginated_posts = normalize_posts(request, posts) # Serialize and paginate posts
+        # Render index.html with normalized posts
         return render(request, "network/index.html", {
             'posts': paginated_posts
         })
-        
+    # If request method is POST then user created a new post    
     elif request.method == 'POST':
-        new_post = Post(content=request.POST['new-post'], user=User.objects.get(pk=request.user.id))
-        new_post.save()
-        return HttpResponseRedirect(reverse('index'))
-    else:
+        new_post = Post(content=request.POST['new-post'], user=User.objects.get(pk=request.user.id)) # Create new post
+        new_post.save() # save post
+        return HttpResponseRedirect(reverse('index')) # Redirect to index
+    # Otherwise return an error
+    else: 
         return render(request, "network/error.html", {
             'message': 'Invalid request type.'
         })
+# Function used to serialize and paginate posts
+def normalize_posts(request, posts):
+    # Serialize posts
+    serialized_posts = [post.serialize() for post in posts]
+    # Paginate posts
+    p = Paginator(serialized_posts, 2)
+    page = request.GET.get('page')
+    paginated_posts = p.get_page(page)
+    # Return normalized posts
+    return paginated_posts 
 
+# View for 'following'
 @login_required
 def following(request):
+    # URL accepts only GET method. Otherwise return an error
     if request.method != 'GET':
         return render(request, "network/error.html", {
             'message': 'Invalid request type.'
         })
     
-    logged_user = User.objects.get(pk=request.user.id)
-    users_followed = logged_user.following.all().values_list('following', flat=True)
-    posts = Post.objects.all().filter(user__in=users_followed)
-
-    serialized_posts = [post.serialize() for post in posts]
-
+    logged_user = User.objects.get(pk=request.user.id) # Get logged user
+    users_followed = logged_user.following.all().values_list('following', flat=True) # Get a list of users_followed
+    posts = Post.objects.all().filter(user__in=users_followed) # Get posts having users followed as posters
+    paginated_posts = normalize_posts(request, posts) # Normalize posts
+    # Render following.html with normalized posts
     return render(request,"network/following.html", {
-        'posts': serialized_posts
+        'posts': paginated_posts
     })
 
+# Edit posts
 @login_required
 def edit_post(request):
+    # URL accepts only PUT method. Otherwise return an error
     if request.method != 'PUT':
         return JsonResponse({"error": "PUT request required."}, status=400)
-
+    # Create post to be edited
     data = json.loads(request.body)
     post = Post.objects.get(pk=data['postId'])
-
+    # Check if the user attempting to edit the post is the one who owns it. Otherwise return an error
     if post.user.id != request.user.id:
         return JsonResponse({"error": "Permission denied. User not authorized to edit this post"})
-    
-    post.content = data['postContent']
-    post.last_update = timezone.now()
-    post.save()
+    # Edit post in database
+    post.content = data['postContent'] # Update post content
+    post.last_update = timezone.now() # Update the 'last updated' field
+    post.save() # Save post
 
-    serialized_post = post.serialize()
-    serialized_post['logged_user'] = request.user.username
-
+    serialized_post = post.serialize() # Serialize post
+    # Return a Json response with the new post state
     return JsonResponse(serialized_post, safe=False)
 
+# Function used to edit user follow status
 @login_required
 def edit_follow(request):
+    # URL accepts only PUT method. Otherwise return an error
     if request.method != 'PUT':
         return JsonResponse({"error": "PUT request required."}, status=400)
 
-    data = json.loads(request.body)
-    user = User.objects.get(pk=request.user.id)
-    
+    data = json.loads(request.body) # Load data
+    user = User.objects.get(pk=request.user.id) # Get logged user object
+    # Get followed user object
     try:
         following = User.objects.get(pk=data['followed'])
     except User.DoesNotExist:
         return JsonResponse({"error": "User not found."})
-
+    # Check update type
     if data['following']:
+        # Create a new follow entry and update response message if a follow is requested
         new_follow = Follow.objects.create(user=user, following=following)
         new_follow.save()
-        return JsonResponse({"message": "user followed."}, status=201)
+        message = "user followed."
     else:
+        # Delete follow entry and update response message if an unfollow is requested
         Follow.objects.all().filter(user=user, following=following).delete()
-        return JsonResponse({"message": "user unfollowed"}, status=201)
+        message = "user unfollowed"
+    # Retun a Json response with OK status
+    return JsonResponse({"message": message}, status=201)
 
-    return JsonResponse({"message": "Follow status successfully updated."}, status=201)
-
+# Function used to edit likes
 @login_required
 def edit_like(request):
+    # URL accepts only PUT method. Otherwise return an error
     if request.method != 'PUT':
         return JsonResponse({"error": "PUT request required."}, status=400)
     
-    data = json.loads(request.body)
-    user = User.objects.get(pk=request.user.id)
-
+    data = json.loads(request.body) # Load data
+    user = User.objects.get(pk=request.user.id) # Get user object for logged user
+    # Get post object
     try:
         post = Post.objects.get(pk=data['postId'])
     except Post.DoesNotExist:
         return JsonResponse({"error": "Post not found."}, status=400)
-    
+    # Update likes based on requested modification
     if data['modification'] == 'like':
         post.likes.add(user)
     elif data['modification'] == 'unlike':
         post.likes.remove(user)
     else:
+        # Return an error if wrong update type is requested
         return JsonResponse({"error": "Invalid modification method."}, status=400)
     
-    post.save()
+    post.save() # Save post
+    # Return a Json response with serialized post
     return JsonResponse(post.serialize(), safe=False)
 
 
 def profile(request, username):
-
+    # URL accepts only PUT method. Otherwise return an error
     if request.method != 'GET':
-        pass
-    # Create user object for requested profile
-    profile = User.objects.get(username=username)
-    # Get profile followers to check if the logged user is following requested profile
-    profile_followers = profile.followers.all().values_list('user', flat=True)
-
-    posts = Post.objects.all().filter(user=profile)
-    serialized_posts = [post.serialize() for post in posts]
+        return render(request, "network/error.html", {
+            'message': 'Invalid request type.'
+        })
+    profile = User.objects.get(username=username) # Create user object for requested profile
+    profile_followers = profile.followers.all().values_list('user', flat=True) # Get profile followers to check if the logged user is following requested profile
+    posts = Post.objects.all().filter(user=profile) # Get posts by user
+    paginated_posts = normalize_posts(request, posts) # Normalize posts
     
     # Render profile page
     return render(request, "network/profile.html", {
@@ -137,9 +154,8 @@ def profile(request, username):
         'num_followers': profile.followers.all().count(), # number of followers
         'num_following': profile.following.all().count(), # number of users followed
         'following': request.user.id in list(profile_followers), # check if logged user is following the requested profile
-        'posts': serialized_posts
+        'posts': paginated_posts
     })
-
 
 def login_view(request):
     if request.method == "POST":
